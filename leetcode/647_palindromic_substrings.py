398deb24e89948bdf82c84ceddfd92c29a5e2edf
Andrew Qiao <andrewqiao2004@gmail.com>
2023-10-05 08:08:08 -0400

    add new solution

--- leetcode/647_palindromic_substrings.py
+++ leetcode/647_palindromic_substrings.py
@@ -13,3 +13,24 @@ def count_palindromes(s):
             start -= 1
             end += 1
     return res
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        count = 0
+        dp = [[False] * len(s) for _ in range(len(s))]
+
+        for i in range(len(s)):
+            dp[i][i] = True
+            count += 1
+        
+        for i in range(len(s) - 1):
+            dp[i][i + 1] = s[i] == s[i + 1]
+            count += dp[i][i + 1]
+        
+        for length in range(3, len(s) + 1):
+            for i in range(len(s) - length + 1):
+                j = i + length - 1
+                dp[i][j] = s[i] == s[j] and dp[i + 1][j - 1]
+                count += dp[i][j]
+        
+        return count
